d202baa3d559df605bfa17d6f84d1c454e62304d
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-01-13 14:39:20 -0300

    :test_tube: Adicionado teste para busca por identificador e atualização por referencia

--- cap2/ecommerce/src/test/java/com/lucasengcomp/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
+++ cap2/ecommerce/src/test/java/com/lucasengcomp/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
@@ -1,5 +1,6 @@
 package com.lucasengcomp.ecommerce.iniciandocomjpa;
 
+import com.lucasengcomp.ecommerce.model.Produto;
 import org.junit.*;
 
 import javax.persistence.EntityManager;
@@ -30,5 +31,21 @@ public class ConsultandoRegistrosTest {
     @After
     public void tearDown() {
         entityManager.close();
+    }
+
+    @Test
+    public void buscarPorIdentificador() {
+        Produto produto = entityManager.find(Produto.class, 1);
+        Assert.assertNotNull(produto);
+        Assert.assertEquals("Kindle", produto.getNome());
+    }
+
+    @Test
+    public void atualizarAReferencia() {
+        Produto produto = entityManager.find(Produto.class, 1);
+        produto.setNome("Microfone Samson");
+        entityManager.refresh(produto);
+
+        Assert.assertEquals("Kindle", produto.getNome());
     }
 }
