078e2869a7c627b60a2ab4330b6992c1e055d7c4
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-01-13 14:44:44 -0300

    :recycle: Adicionado teste para busca por identificador e atualização por referencia

--- cap2/ecommerce/src/test/java/com/lucasengcomp/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
+++ cap2/ecommerce/src/test/java/com/lucasengcomp/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
@@ -1,37 +1,10 @@
 package com.lucasengcomp.ecommerce.iniciandocomjpa;
 
 import com.lucasengcomp.ecommerce.model.Produto;
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Test;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
-public class ConsultandoRegistrosTest {
-
-    private static EntityManagerFactory entityManagerFactory;
-    private EntityManager entityManager;
-
-    @BeforeClass
-    public static void setUpBeforeClass() {
-        entityManagerFactory = Persistence
-                .createEntityManagerFactory("Ecommerce-PU");
-    }
-
-    @AfterClass
-    public static void tearDownAfterClass() {
-        entityManagerFactory.close();
-    }
-
-    @Before
-    public void setUp() {
-        entityManager = entityManagerFactory.createEntityManager();
-    }
-
-    @After
-    public void tearDown() {
-        entityManager.close();
-    }
+public class ConsultandoRegistrosTest extends EntityManagerTest {
 
     @Test
     public void buscarPorIdentificador() {
