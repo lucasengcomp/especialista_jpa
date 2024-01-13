90d30c1bebe936e886e760e3ed0178150a15a405
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-01-13 16:20:13 -0300

    :test_tube: Removendo objetos com o método remove

--- cap2/ecommerce/target/classes/META-INF/banco-de-dados/dados-iniciais.sql
+++ cap2/ecommerce/target/classes/META-INF/banco-de-dados/dados-iniciais.sql
@@ -1,1 +1,2 @@
 insert into Produto (id, nome, preco, descricao) values (1, 'Kindle', 499.0, 'Conheça o novo Kindle, agora com iluminação embutida ajustável, que permite que você leia em ambientes abertos ou fechados, a qualquer hora do dia.');
+insert into Produto (id, nome, preco, descricao) values (3, 'Camera GoPro', 1499.0, 'Uma excelente câmera para gravar seus sports!');
