0b48104742db073c9babbc1d84191c444a8cb4c2
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-01-13 12:17:22 -0300

    :sparkles: Iniciando unidade de persistencia manualmente

---
+++ cap2/ecommerce/src/main/java/com/lucasengcomp/ecommerce/model/util/IniciarUnidadeDePersistencia.java
@@ -0,0 +1,21 @@
+package com.lucasengcomp.ecommerce.model.util;
+
+import com.lucasengcomp.ecommerce.model.Produto;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class IniciarUnidadeDePersistencia {
+
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Ecommerce-PU");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        Produto produto = entityManager.find(Produto.class, 1);
+        System.out.println(produto.getNome());
+
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+}
