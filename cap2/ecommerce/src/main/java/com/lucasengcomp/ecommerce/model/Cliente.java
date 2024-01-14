0f2ee246e133ca2688bcbfbf35c1fecc3109322d
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-01-14 09:10:16 -0300

    :sparkles: Criado entidade Cliente e insert

---
+++ cap2/ecommerce/src/main/java/com/lucasengcomp/ecommerce/model/Cliente.java
@@ -0,0 +1,51 @@
+package com.lucasengcomp.ecommerce.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Objects;
+
+
+@Entity
+public class Cliente {
+
+    @Id
+    private Long id;
+    private String nome;
+
+    public Cliente() {
+    }
+
+    public Cliente(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(id, cliente.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
--- cap2/ecommerce/target/classes/META-INF/banco-de-dados/dados-iniciais.sql
+++ cap2/ecommerce/target/classes/META-INF/banco-de-dados/dados-iniciais.sql
@@ -1,2 +1,5 @@
 insert into Produto (id, nome, preco, descricao) values (1, 'Kindle', 499.0, 'Conheça o novo Kindle, agora com iluminação embutida ajustável, que permite que você leia em ambientes abertos ou fechados, a qualquer hora do dia.');
 insert into Produto (id, nome, preco, descricao) values (3, 'Camera GoPro', 1499.0, 'Uma excelente câmera para gravar seus sports!');
+
+insert into Cliente (id, nome) values (1, 'Lucas Galvao');
+insert into Cliente (id, nome) values (1, 'Freddie Mercury');
