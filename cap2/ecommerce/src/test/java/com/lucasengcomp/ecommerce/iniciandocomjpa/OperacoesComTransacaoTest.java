41a9f2db50d974103f314e1fd4895dbae3dd56d0
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-01-14 08:28:08 -0300

    :test_tube: Atualizando objetos com o método merge

--- cap2/ecommerce/src/test/java/com/lucasengcomp/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
+++ cap2/ecommerce/src/test/java/com/lucasengcomp/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
@@ -10,6 +10,25 @@ import java.math.BigDecimal;
 public class OperacoesComTransacaoTest extends EntityManagerTest {
 
     @Test
+    public void atualizarObjeto() {
+        Produto produto = new Produto();
+        produto.setId(1);
+        produto.setNome("Kindle Paperwhite");
+        produto.setDescricao("Conheça o novo kindle");
+        produto.setPreco(new BigDecimal(599));
+
+        entityManager.getTransaction().begin();
+        entityManager.merge(produto);
+        entityManager.getTransaction().commit();
+        entityManager.clear();
+
+        Produto produtoVerificado = entityManager.find(Produto.class, produto.getId());
+        Assert.assertNotNull(produtoVerificado);
+        Assert.assertEquals("Kindle Paperwhite", produto.getNome());
+
+    }
+
+    @Test
     public void removerObjeto() {
         Produto produto = entityManager.find(Produto.class, 3);
 
