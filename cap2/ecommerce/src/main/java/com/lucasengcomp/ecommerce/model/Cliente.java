1aafca55e7a1d0e662a50c33ce2153f73bc183c3
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-01-15 07:54:25 -0300

    :test: Mapeando enumerações com @Enumerated

--- cap2/ecommerce/src/main/java/com/lucasengcomp/ecommerce/model/Cliente.java
+++ cap2/ecommerce/src/main/java/com/lucasengcomp/ecommerce/model/Cliente.java
@@ -5,9 +5,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 
 @Getter
@@ -21,5 +19,7 @@ public class Cliente {
     @EqualsAndHashCode.Include
     private Integer id;
     private String nome;
+
+    @Enumerated(EnumType.STRING)
     private SexoCliente sexoCliente;
 }
