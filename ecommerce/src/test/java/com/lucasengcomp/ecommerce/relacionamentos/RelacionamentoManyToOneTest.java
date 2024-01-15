dcc5163cdf3b1b455587c2df85e861c2efcfa40d
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-01-15 11:59:15 -0300

    :test_tube: Mapeando relacionamentos muito-para-um com @ManyToOne

---
+++ ecommerce/src/test/java/com/lucasengcomp/ecommerce/relacionamentos/RelacionamentoManyToOneTest.java
@@ -0,0 +1,34 @@
+package com.lucasengcomp.ecommerce.relacionamentos;
+
+import com.lucasengcomp.ecommerce.EntityManagerTest;
+import com.lucasengcomp.ecommerce.model.Cliente;
+import com.lucasengcomp.ecommerce.model.Pedido;
+import com.lucasengcomp.ecommerce.model.enums.StatusPedido;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class RelacionamentoManyToOneTest extends EntityManagerTest {
+
+    @Test
+    public void verificarRelacionamento() {
+        Cliente cliente = entityManager.find(Cliente.class, 1);
+
+        Pedido pedido = new Pedido();
+
+        pedido.setStatusPedido(StatusPedido.AGUARDANDO);
+        pedido.setDataPedido(LocalDateTime.now());
+        pedido.setTotal(BigDecimal.TEN);
+        pedido.setCliente(cliente);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(pedido);
+        entityManager.getTransaction().commit();
+        entityManager.clear();
+
+        Pedido pedidoPersistido = entityManager.find(Pedido.class, pedido.getId());
+        Assert.assertNotNull(pedidoPersistido.getCliente());
+    }
+}
