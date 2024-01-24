c47f1e9584719f1c0884dc8a8928a1f4ee107f56
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-01-23 21:12:28 -0300

    :test_tube: Entendendo alguns detalhes de @JoinTable

--- ecommerce/src/main/java/com/lucasengcomp/ecommerce/model/Produto.java
+++ ecommerce/src/main/java/com/lucasengcomp/ecommerce/model/Produto.java
@@ -39,8 +39,10 @@ public class Produto extends EntidadeBaseInteger {
 
     @ManyToMany
     @JoinTable(name = "produto_categoria",
-            joinColumns = @JoinColumn(name = "produto_id"),
-            inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+            joinColumns = @JoinColumn(name = "produto_id",
+                    foreignKey = @ForeignKey(name = "fk_produto_categoria_produto"), nullable = false),
+            foreignKey = @ForeignKey(name = "fk_produto_categoria_categoria"),
+            inverseJoinColumns = @JoinColumn(name = "categoria_id", nullable = false))
     private List<Categoria> categorias;
 
     @OneToOne(mappedBy = "produto")
