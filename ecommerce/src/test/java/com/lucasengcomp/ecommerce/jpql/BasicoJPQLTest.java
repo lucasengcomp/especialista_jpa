5c5047818292c8b42aa185b1dc4131f570bf472b
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-01-25 10:03:55 -0300

    :test_tube: Selecionando um atributo da entidade como retorno da consulta

--- ecommerce/src/test/java/com/lucasengcomp/ecommerce/jpql/BasicoJPQLTest.java
+++ ecommerce/src/test/java/com/lucasengcomp/ecommerce/jpql/BasicoJPQLTest.java
@@ -1,7 +1,9 @@
 package com.lucasengcomp.ecommerce.jpql;
 
 import com.lucasengcomp.ecommerce.EntityManagerTest;
+import com.lucasengcomp.ecommerce.model.Cliente;
 import com.lucasengcomp.ecommerce.model.Pedido;
+import com.lucasengcomp.ecommerce.model.Produto;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -10,6 +12,24 @@ import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class BasicoJPQLTest extends EntityManagerTest {
+
+    @Test
+    public void selecionarUmAtributoParaRetorno() {
+        String jpql = "SELECT p FROM Produto p";
+
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(jpql, Produto.class);
+
+        List<Produto> produtos = typedQuery.getResultList();
+
+        String jpqlCliente = "SELECT p.cliente FROM Pedido p";
+
+        TypedQuery<Cliente> typedQueryCliente = entityManager.createQuery(jpqlCliente, Cliente.class);
+        List<Cliente> clientes = typedQueryCliente.getResultList();
+
+        System.out.println(Cliente.class.equals(clientes.get(0).getClass()));
+
+        Assert.assertTrue(Cliente.class.equals(clientes.get(0).getClass()));
+    }
 
     @Test
     public void buscarPorIdentificador() {
