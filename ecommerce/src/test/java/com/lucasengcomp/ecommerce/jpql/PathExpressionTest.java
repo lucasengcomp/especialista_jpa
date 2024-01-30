f7281c93205ead378ed5f820bb5ecc67c040d4c5
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-01-30 18:37:31 -0300

    :test_tube: Busca por Path expressions

---
+++ ecommerce/src/test/java/com/lucasengcomp/ecommerce/jpql/PathExpressionTest.java
@@ -0,0 +1,21 @@
+package com.lucasengcomp.ecommerce.jpql;
+
+import com.lucasengcomp.ecommerce.EntityManagerTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class PathExpressionTest extends EntityManagerTest {
+
+    @Test
+    public void buscaClientePorPathExpressions() {
+        String jpql = "select p from Pedido p where p.cliente.nome = 'Lucas Galvao'";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+}
