0a4a66e808ef7aaffa7eec8002dea251c25de825
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-01-30 19:48:30 -0300

    :test_tube: Exercício busca produto por id

--- ecommerce/src/test/java/com/lucasengcomp/ecommerce/jpql/PathExpressionTest.java
+++ ecommerce/src/test/java/com/lucasengcomp/ecommerce/jpql/PathExpressionTest.java
@@ -10,6 +10,17 @@ import java.util.List;
 public class PathExpressionTest extends EntityManagerTest {
 
     @Test
+    public void buscaProdutoPorIdViaPathExpressions() {
+        String jpql = "SELECT ped FROM Pedido ped JOIN ped.itens item JOIN item.produto prod WHERE prod.id = 1";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+        Assert.assertTrue(lista.size() == 2);
+    }
+
+    @Test
     public void buscaClientePorPathExpressions() {
         String jpql = "select p from Pedido p where p.cliente.nome = 'Lucas Galvao'";
 
