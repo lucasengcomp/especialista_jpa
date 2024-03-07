b7db7946cd004272999633fc0cfcfd13fde71f4e
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-03-07 10:40:55 -0300

    :test_tube: Usando expressão condicional like e concat

---
+++ ecommerce/src/test/java/com/lucasengcomp/ecommerce/jpql/ExpressoesCondicionaisTest.java
@@ -0,0 +1,59 @@
+package com.lucasengcomp.ecommerce.jpql;
+
+import com.lucasengcomp.ecommerce.EntityManagerTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class ExpressoesCondicionaisTest extends EntityManagerTest {
+
+    @Test
+    public void usarExpressaoCondicionalLikeConcatAmbos() {
+        String jpql = "SELECT c from Cliente c WHERE c.nome LIKE CONCAT('%', :nome, '%')";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        typedQuery.setParameter("nome", "Lucas");
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void usarExpressaoCondicionalLikeConcatInicio() {
+        String jpql = "SELECT c from Cliente c WHERE c.nome LIKE CONCAT('%', :nome)";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        typedQuery.setParameter("nome", "Lucas");
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertTrue(lista.isEmpty());
+    }
+
+    @Test
+    public void usarExpressaoCondicionalLikeConcatFim() {
+        String jpql = "SELECT c from Cliente c WHERE c.nome LIKE CONCAT(:nome, '%')";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        typedQuery.setParameter("nome", "Lucas");
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void usarExpressaoCondicionalLike() {
+        String jpql = "SELECT c from Cliente c WHERE c.nome LIKE :nome";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        typedQuery.setParameter("nome", "Lucas%");
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+}
