af4a9eab6e8bcfb283b1aab2a78f2eba00e0d122
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-03-07 20:15:45 -0300

    :test_tube: Usando funções para datas

---
+++ ecommerce/src/test/java/com/lucasengcomp/ecommerce/jpql/FuncoesDataTest.java
@@ -0,0 +1,47 @@
+package com.lucasengcomp.ecommerce.jpql;
+
+import com.lucasengcomp.ecommerce.EntityManagerTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class FuncoesDataTest extends EntityManagerTest {
+
+    @Test
+    public void aplicarFuncaoDataHHMMss() {
+        String jpql = "SELECT hour(p.dataCriacao), minute(p.dataCriacao), second(p.dataCriacao) FROM Pedido p";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + " | " + arr[1] + " | " + arr[2]));
+    }
+
+    @Test
+    public void aplicarFuncaoDataYYYYMMDD() {
+        String jpql = "SELECT year(p.dataCriacao), month(p.dataCriacao), day(p.dataCriacao) FROM Pedido p";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + " | " + arr[1] + " | " + arr[2]));
+    }
+
+    @Test
+    public void aplicarFuncaoData() {
+        String jpql = "SELECT current_date, current_time, current_timestamp FROM Pedido p";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + " | " + arr[1] + " | " + arr[2]));
+    }
+}
