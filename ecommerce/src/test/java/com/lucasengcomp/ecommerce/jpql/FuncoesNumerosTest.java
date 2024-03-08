96ce1b76deb5bac7d0156cf53c534ec0965db70e
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-03-08 16:38:23 -0300

    :test_tube: Usando funções matemáticas/agregação

--- ecommerce/src/test/java/com/lucasengcomp/ecommerce/jpql/FuncoesNumerosTest.java
+++ ecommerce/src/test/java/com/lucasengcomp/ecommerce/jpql/FuncoesNumerosTest.java
@@ -10,6 +10,66 @@ import java.util.List;
 public class FuncoesNumerosTest extends EntityManagerTest {
 
     @Test
+    public void aplicarFuncaoSUM() {
+        String jpql = "SELECT SUM(p.total) FROM Pedido p";
+
+        TypedQuery<Number> typedQuery = entityManager.createQuery(jpql, Number.class);
+
+        List<Number> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(obj -> System.out.println(obj));
+    }
+
+    @Test
+    public void aplicarFuncaoMAX() {
+        String jpql = "SELECT MAX(p.total) FROM Pedido p";
+
+        TypedQuery<Number> typedQuery = entityManager.createQuery(jpql, Number.class);
+
+        List<Number> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(obj -> System.out.println(obj));
+    }
+
+    @Test
+    public void aplicarFuncaoMIN() {
+        String jpql = "SELECT MIN(p.total) FROM Pedido p";
+
+        TypedQuery<Number> typedQuery = entityManager.createQuery(jpql, Number.class);
+
+        List<Number> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(obj -> System.out.println(obj));
+    }
+
+    @Test
+    public void aplicarFuncaoCOUNT() {
+        String jpql = "SELECT COUNT(p.id) FROM Pedido p";
+
+        TypedQuery<Number> typedQuery = entityManager.createQuery(jpql, Number.class);
+
+        List<Number> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(obj -> System.out.println(obj));
+    }
+
+    @Test
+    public void aplicarFuncaoAVG() {
+        String jpql = "SELECT AVG(p.total) FROM Pedido p";
+
+        TypedQuery<Number> typedQuery = entityManager.createQuery(jpql, Number.class);
+
+        List<Number> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(obj -> System.out.println(obj));
+    }
+
+    @Test
     public void aplicarFuncaoNumeroAbsoluto() {
         String jpql = "SELECT abs(p.total), mod(3, 2), sqrt(9) FROM Pedido p";
 
