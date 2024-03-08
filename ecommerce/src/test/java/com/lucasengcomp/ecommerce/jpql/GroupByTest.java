c1bda96f2c0c9a53196e35d29d96015ecdf73566
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-03-08 18:17:09 -0300

    :test_tube: Agrupando o registros com group by

---
+++ ecommerce/src/test/java/com/lucasengcomp/ecommerce/jpql/GroupByTest.java
@@ -0,0 +1,53 @@
+package com.lucasengcomp.ecommerce.jpql;
+
+import com.lucasengcomp.ecommerce.EntityManagerTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class GroupByTest extends EntityManagerTest {
+
+    @Test
+    public void agruparResultadoTotalVendasPorCategoria() {
+        String jpql = "SELECT c.nome, SUM(ip.precoProduto) FROM ItemPedido ip " +
+                " JOIN ip.produto pro join pro.categorias c " +
+                " GROUP BY c.id";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse("A lista de resultados não deve estar vazia", lista.isEmpty());
+
+        lista.forEach(obj -> System.out.println(obj[0] + " - " + obj[1]));
+    }
+
+    @Test
+    public void agruparResultadoTotalVendasPorMes() {
+        String jpql = "SELECT DATE_FORMAT(p.dataCriacao, '%Y %M'), SUM(p.total) " +
+                "FROM Pedido p " +
+                "GROUP BY DATE_FORMAT(p.dataCriacao, '%Y %M')";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse("A lista de resultados não deve estar vazia", lista.isEmpty());
+
+        lista.forEach(obj -> System.out.println(obj[0] + " - " + obj[1]));
+    }
+
+    @Test
+    public void agruparResultadoProdutoPorCategoria() {
+        String jpql = " SELECT c.nome, COUNT(p.id) FROM Categoria c " +
+                " JOIN c.produtos p" +
+                " GROUP BY c.id ";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(obj -> System.out.println(obj[0] + " - " + obj[1]));
+    }
+}
