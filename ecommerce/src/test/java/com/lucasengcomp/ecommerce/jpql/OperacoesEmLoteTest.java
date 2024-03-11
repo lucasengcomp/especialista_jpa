0acddf4971d8d63ce43de1883f73de984d3ab05e
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-03-11 13:56:06 -0300

    :test_tube: Atualizando objetos em lote

--- ecommerce/src/test/java/com/lucasengcomp/ecommerce/jpql/OperacoesEmLoteTest.java
+++ ecommerce/src/test/java/com/lucasengcomp/ecommerce/jpql/OperacoesEmLoteTest.java
@@ -4,6 +4,7 @@ import com.lucasengcomp.ecommerce.EntityManagerTest;
 import com.lucasengcomp.ecommerce.model.Produto;
 import org.junit.Test;
 
+import javax.persistence.Query;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,6 +15,33 @@ import java.util.stream.Collectors;
 
 public class OperacoesEmLoteTest extends EntityManagerTest {
     private static final int LIMITE_INSERCOES = 4;
+
+    @Test
+    public void atualizarEmLotePrecoComId() {
+        entityManager.getTransaction().begin();
+
+        String jpql = "update Produto p set p.preco = p.preco + (p.preco * :valor) " +
+                " where exists (select 1 from p.categorias c2 where c2.id = :categoria)";
+
+        Query query = entityManager.createQuery(jpql);
+        query.setParameter("categoria", 2);
+        query.setParameter("valor", new BigDecimal("0.1"));
+        query.executeUpdate();
+
+        entityManager.getTransaction().commit();
+    }
+
+   @Test
+    public void atualizarEmLotePrecoComIdEntre1e10() {
+        entityManager.getTransaction().begin();
+
+        String jpql = "UPDATE Produto p SET p.preco = p.preco + 1 WHERE id BETWEEN 1 AND 10 ";
+
+        Query query = entityManager.createQuery(jpql);
+        query.executeUpdate();
+
+        entityManager.getTransaction().commit();
+    }
 
     @Test
     public void inserirEmLote() throws IOException {
