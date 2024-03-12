d7c74f2c93d62a43dcad9ba7d934587ee9419973
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-03-12 19:04:49 -0300

    :test_tube: Selecionando um atributo da entidade como retorno da consulta

--- ecommerce/src/test/java/com/lucasengcomp/ecommerce/criteria/BasicoCriteriaTest.java
+++ ecommerce/src/test/java/com/lucasengcomp/ecommerce/criteria/BasicoCriteriaTest.java
@@ -1,6 +1,7 @@
 package com.lucasengcomp.ecommerce.criteria;
 
 import com.lucasengcomp.ecommerce.EntityManagerTest;
+import com.lucasengcomp.ecommerce.model.Cliente;
 import com.lucasengcomp.ecommerce.model.Pedido;
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,8 +10,39 @@ import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import java.math.BigDecimal;
 
 public class BasicoCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void selecionarUmAtributoParaRetorno() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<BigDecimal> criteriaQuery = criteriaBuilder.createQuery(BigDecimal.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.select(root.get("total"));
+        criteriaQuery.where(criteriaBuilder.equal(root.get("id"), 1));
+
+        TypedQuery<BigDecimal> typedQuery = entityManager.createQuery(criteriaQuery);
+        BigDecimal total = typedQuery.getSingleResult();
+
+        Assert.assertEquals(new BigDecimal("2398.00"), total);
+    }
+
+     @Test
+    public void selecionarUmCliente() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Cliente> criteriaQuery = criteriaBuilder.createQuery(Cliente.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.select(root.get("cliente"));
+        criteriaQuery.where(criteriaBuilder.equal(root.get("id"), 1));
+
+        TypedQuery<Cliente> typedQuery = entityManager.createQuery(criteriaQuery);
+        Cliente cliente = typedQuery.getSingleResult();
+
+        Assert.assertEquals("Lucas Galvao", cliente.getNome());
+    }
 
     @Test
     public void buscarPorIdentificador() {
