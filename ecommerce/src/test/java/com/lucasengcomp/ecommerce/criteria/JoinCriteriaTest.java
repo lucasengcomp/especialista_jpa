1dceb240450a76a3bcbe4f674745a9aa32f76203
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-03-14 12:13:25 -0300

    :test_tube: Usando a cláusula on no join

--- ecommerce/src/test/java/com/lucasengcomp/ecommerce/criteria/JoinCriteriaTest.java
+++ ecommerce/src/test/java/com/lucasengcomp/ecommerce/criteria/JoinCriteriaTest.java
@@ -17,6 +17,22 @@ import java.util.List;
 public class JoinCriteriaTest extends EntityManagerTest {
 
     @Test
+    public void fazerJoinComON() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+        Join<Pedido, Pagamento> joinPagamento = root.join("pagamento");
+
+        joinPagamento.on(criteriaBuilder.equal(joinPagamento.get("status"), StatusPagamento.PROCESSANDO));
+        criteriaQuery.select(root);
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Pedido> lista = typedQuery.getResultList();
+
+        Assert.assertTrue(lista.size() == 1);
+    }
+
+    @Test
     public void fazerJoinPedidoPagamento() {
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Pagamento> criteriaQuery = criteriaBuilder.createQuery(Pagamento.class);
