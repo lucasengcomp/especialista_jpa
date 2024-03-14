ee9c8201037d89954c1049a29a201f2dae9340b5
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-03-14 12:08:02 -0300

    :test_tube: Fazendo inner join entre as entidades

---
+++ ecommerce/src/test/java/com/lucasengcomp/ecommerce/criteria/JoinCriteriaTest.java
@@ -0,0 +1,35 @@
+package com.lucasengcomp.ecommerce.criteria;
+
+import com.lucasengcomp.ecommerce.EntityManagerTest;
+import com.lucasengcomp.ecommerce.model.Pedido;
+import com.lucasengcomp.ecommerce.model.abstractclass.Pagamento;
+import com.lucasengcomp.ecommerce.model.enums.StatusPagamento;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class JoinCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void fazerJoinPedidoPagamento() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pagamento> criteriaQuery = criteriaBuilder.createQuery(Pagamento.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+        Join<Pedido, Pagamento> joinPagamento = root.join("pagamento");
+
+        criteriaQuery.select(joinPagamento);
+        criteriaQuery.where(criteriaBuilder.equal(joinPagamento.get("status"), StatusPagamento.PROCESSANDO));
+
+
+        TypedQuery<Pagamento> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Pagamento> lista = typedQuery.getResultList();
+
+        Assert.assertTrue(lista.size() == 1);
+    }
+}
