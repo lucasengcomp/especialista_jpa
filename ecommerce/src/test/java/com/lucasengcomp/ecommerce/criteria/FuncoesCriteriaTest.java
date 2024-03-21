162a4360bcd574d3648fd2fea99b53a7ba758990
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-03-21 19:55:15 -0300

    :test_tube: Usando função para datas. Atualizado script. Adicionado campo dataVencimento em PagamentoBoleto

--- ecommerce/src/test/java/com/lucasengcomp/ecommerce/criteria/FuncoesCriteriaTest.java
+++ ecommerce/src/test/java/com/lucasengcomp/ecommerce/criteria/FuncoesCriteriaTest.java
@@ -1,18 +1,57 @@
 package com.lucasengcomp.ecommerce.criteria;
 
 import com.lucasengcomp.ecommerce.EntityManagerTest;
-import com.lucasengcomp.ecommerce.model.Cliente;
-import com.lucasengcomp.ecommerce.model.Cliente_;
+import com.lucasengcomp.ecommerce.model.*;
+import com.lucasengcomp.ecommerce.model.abstractclass.Pagamento;
+import com.lucasengcomp.ecommerce.model.enums.StatusPedido;
 import org.junit.Assert;
 import org.junit.Test;
 
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class FuncoesCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void aplicarFuncaoData() {
+        // current_date, current_time, current_timestamp
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+        Join<Pedido, Pagamento> joinPagamento = root.join(Pedido_.pagamento);
+        Join<Pedido, PagamentoBoleto> joinPagamentoBoleto = criteriaBuilder
+                .treat(joinPagamento, PagamentoBoleto.class);
+
+        criteriaQuery.multiselect(
+                root.get(Pedido_.id),
+                criteriaBuilder.currentDate(),
+                criteriaBuilder.currentTime(),
+                criteriaBuilder.currentTimestamp()
+        );
+
+        criteriaQuery.where(
+                criteriaBuilder.between(criteriaBuilder.currentDate(),
+                        root.get(Pedido_.dataCriacao).as(java.sql.Date.class),
+                        joinPagamentoBoleto.get(PagamentoBoleto_.dataVencimento).as(java.sql.Date.class)),
+                criteriaBuilder.equal(root.get(Pedido_.status), StatusPedido.AGUARDANDO)
+        );
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(
+                arr[0]
+                        + ", current_date: " + arr[1]
+                        + ", current_time: " + arr[2]
+                        + ", current_timestamp: " + arr[3]));
+    }
 
     @Test
     public void aplicarFuncaoString() {
--- ecommerce/target/generated-sources/annotations/com/lucasengcomp/ecommerce/model/PagamentoBoleto_.java
+++ ecommerce/target/generated-sources/annotations/com/lucasengcomp/ecommerce/model/PagamentoBoleto_.java
@@ -1,5 +1,6 @@
 package com.lucasengcomp.ecommerce.model;
 
+import java.time.LocalDate;
 import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
@@ -8,8 +9,10 @@ import javax.persistence.metamodel.StaticMetamodel;
 @StaticMetamodel(PagamentoBoleto.class)
 public abstract class PagamentoBoleto_ extends com.lucasengcomp.ecommerce.model.abstractclass.Pagamento_ {
 
+	public static volatile SingularAttribute<PagamentoBoleto, LocalDate> dataVencimento;
 	public static volatile SingularAttribute<PagamentoBoleto, String> codigoBarras;
 
+	public static final String DATA_VENCIMENTO = "dataVencimento";
 	public static final String CODIGO_BARRAS = "codigoBarras";
 
 }
