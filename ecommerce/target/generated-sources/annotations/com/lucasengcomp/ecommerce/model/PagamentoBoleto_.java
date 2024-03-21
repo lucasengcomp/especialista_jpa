d2fb37b6ab68c2d34386cada511548841b8c8dd4
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-03-21 11:36:51 -0300

    :test_tube: Tipagem forte com metamodel

---
+++ ecommerce/target/generated-sources/annotations/com/lucasengcomp/ecommerce/model/PagamentoBoleto_.java
@@ -0,0 +1,16 @@
+package com.lucasengcomp.ecommerce.model;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(PagamentoBoleto.class)
+public abstract class PagamentoBoleto_ extends com.lucasengcomp.ecommerce.model.abstractclass.Pagamento_ {
+
+	public static volatile SingularAttribute<PagamentoBoleto, String> codigoBarras;
+
+	public static final String CODIGO_BARRAS = "codigoBarras";
+
+}
+
---
+++ ecommerce/target/generated-sources/annotations/com/lucasengcomp/ecommerce/model/PagamentoCartao_.java
@@ -0,0 +1,16 @@
+package com.lucasengcomp.ecommerce.model;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(PagamentoCartao.class)
+public abstract class PagamentoCartao_ extends com.lucasengcomp.ecommerce.model.abstractclass.Pagamento_ {
+
+	public static volatile SingularAttribute<PagamentoCartao, String> numeroCartao;
+
+	public static final String NUMERO_CARTAO = "numeroCartao";
+
+}
+
