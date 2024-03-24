48045ea447a099019720f290d19b6504549e271c
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-03-24 10:12:57 -0300

    :test_tube: Usando a expressão case

--- ecommerce/src/test/java/com/lucasengcomp/ecommerce/criteria/ExpressoesCondicionaisTest.java
+++ ecommerce/src/test/java/com/lucasengcomp/ecommerce/criteria/ExpressoesCondicionaisTest.java
@@ -17,6 +17,29 @@ import java.util.List;
 public class ExpressoesCondicionaisTest extends EntityManagerTest {
 
     @Test
+    public void usarExpressaoCase() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.multiselect(
+                root.get(Pedido_.id),
+                criteriaBuilder.selectCase(root.get(Pedido_.pagamento).type().as(String.class))
+                        .when("boleto", "Foi pago com boleto.") //boleto da annotation @DiscriminatorValue
+                        .when("cartao", "Foi pago com cartão") //cartao da annotation @DiscriminatorValue
+                        .otherwise("Não identificado")
+        );
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
+    }
+
+
+    @Test
     public void usarExpressaoDESC() {
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Cliente> criteriaQuery = criteriaBuilder.createQuery(Cliente.class);
