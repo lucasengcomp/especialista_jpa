398d96445cd6e7bff3386e9dc7482c80c058cb3d
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-03-26 18:38:32 -0300

    :test_tube: Atualizando objetos em lote

---
+++ ecommerce/src/test/java/com/lucasengcomp/ecommerce/criteria/OperacoesEmLoteCriteriaTest.java
@@ -0,0 +1,40 @@
+package com.lucasengcomp.ecommerce.criteria;
+
+import com.lucasengcomp.ecommerce.EntityManagerTest;
+import com.lucasengcomp.ecommerce.model.Categoria;
+import com.lucasengcomp.ecommerce.model.Categoria_;
+import com.lucasengcomp.ecommerce.model.Produto;
+import com.lucasengcomp.ecommerce.model.Produto_;
+import org.junit.Test;
+
+import javax.persistence.Query;
+import javax.persistence.criteria.*;
+import java.math.BigDecimal;
+
+public class OperacoesEmLoteCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void atualizarEmLote() {
+        entityManager.getTransaction().begin();
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaUpdate<Produto> criteriaUpdate = criteriaBuilder.createCriteriaUpdate(Produto.class);
+        Root<Produto> root = criteriaUpdate.from(Produto.class);
+
+        criteriaUpdate.set(root.get(Produto_.preco),
+                criteriaBuilder.prod(root.get(Produto_.preco), new BigDecimal("1.1")));
+
+        Subquery<Integer> subquery = criteriaUpdate.subquery(Integer.class);
+        Root<Produto> subqueryRoot = subquery.correlate(root);
+        Join<Produto, Categoria> joinCategoria = subqueryRoot.join(Produto_.categorias);
+        subquery.select(criteriaBuilder.literal(1));
+        subquery.where(criteriaBuilder.equal(joinCategoria.get(Categoria_.id), 2));
+
+        criteriaUpdate.where(criteriaBuilder.exists(subquery));
+
+        Query query = entityManager.createQuery(criteriaUpdate);
+        query.executeUpdate();
+
+        entityManager.getTransaction().commit();
+    }
+}
