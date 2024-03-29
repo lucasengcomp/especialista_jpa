27ffa1a495e10b494e88e8b98226df8c2539b2c1
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-03-29 09:03:10 -0300

    :test_tube: Resolvendo o problema do N+1

--- ecommerce/src/main/java/com/lucasengcomp/ecommerce/model/Pedido.java
+++ ecommerce/src/main/java/com/lucasengcomp/ecommerce/model/Pedido.java
@@ -10,22 +10,43 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
-import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
 
-
 @Getter
 @Setter
-@EntityListeners({ GerarNotaFiscalListener.class, GenericoListener.class })
 @Entity
+@NamedEntityGraphs({
+        @NamedEntityGraph(
+                name = "Pedido.dadosEssencias",
+                attributeNodes = {
+                        @NamedAttributeNode("dataCriacao"),
+                        @NamedAttributeNode("status"),
+                        @NamedAttributeNode("total"),
+                        @NamedAttributeNode(
+                                value = "cliente",
+                                subgraph = "cli"
+                        )
+                },
+                subgraphs = {
+                        @NamedSubgraph(
+                                name = "cli",
+                                attributeNodes = {
+                                        @NamedAttributeNode("nome"),
+                                        @NamedAttributeNode("cpf")
+                                }
+                        )
+                }
+        )
+})
+@EntityListeners({GerarNotaFiscalListener.class, GenericoListener.class})
 @Table(name = "pedido")
 public class Pedido extends EntidadeBaseInteger {
 
+    @NotNull
     @PastOrPresent
     @Column(name = "data_criacao", updatable = false, nullable = false)
     private LocalDateTime dataCriacao;
@@ -35,10 +56,11 @@ public class Pedido extends EntidadeBaseInteger {
     private LocalDateTime dataUltimaAtualizacao;
 
     @Past
-    @NotNull
     @Column(name = "data_conclusao")
     private LocalDateTime dataConclusao;
 
+    @NotNull
+    @Positive
     @Column(nullable = false)
     private BigDecimal total;
 
@@ -57,16 +79,70 @@ public class Pedido extends EntidadeBaseInteger {
     @JoinColumn(name = "cliente_id", nullable = false, foreignKey = @ForeignKey(name = "fk_pedido_cliente"))
     private Cliente cliente;
 
+    @NotEmpty
     @OneToMany(mappedBy = "pedido")
     private List<ItemPedido> itens;
 
     @Embedded
     private EnderecoEntregaPedido enderecoEntrega;
 
+    //    @Setter(AccessLevel.NONE)
+//    @Getter(AccessLevel.NONE)
+//    @Transient
+//    private PersistentAttributeInterceptor persistentAttributeInterceptor;
+//
+//    public NotaFiscal getNotaFiscal() {
+//        if (this.persistentAttributeInterceptor != null) {
+//            return (NotaFiscal) persistentAttributeInterceptor
+//                    .readObject(this, "notaFiscal", this. notaFiscal);
+//        }
+//
+//        return this.notaFiscal;
+//    }
+//
+//    public void setNotaFiscal(NotaFiscal notaFiscal) {
+//        if (this.persistentAttributeInterceptor != null) {
+//            this.notaFiscal = (NotaFiscal) persistentAttributeInterceptor
+//                    .writeObject(this, "notaFiscal", this.notaFiscal, notaFiscal);
+//        } else {
+//            this.notaFiscal = notaFiscal;
+//        }
+//    }
+//
+//    public Pagamento getPagamento() {
+//        if (this.persistentAttributeInterceptor != null) {
+//            return (Pagamento) persistentAttributeInterceptor
+//                    .readObject(this, "pagamento", this.pagamento);
+//        }
+//
+//        return this.pagamento;
+//    }
+//
+//    public void setPagamento(Pagamento pagamento) {
+//        if (this.persistentAttributeInterceptor != null) {
+//            this.pagamento = (Pagamento) persistentAttributeInterceptor
+//                    .writeObject(this, "pagamento", this.pagamento, pagamento);
+//        } else {
+//            this.pagamento = pagamento;
+//        }
+//    }
+//
+//    @Override
+//    public PersistentAttributeInterceptor $$_hibernate_getInterceptor() {
+//        return this.persistentAttributeInterceptor;
+//    }
+//
+//    @Override
+//    public void $$_hibernate_setInterceptor(PersistentAttributeInterceptor persistentAttributeInterceptor) {
+//        this.persistentAttributeInterceptor = persistentAttributeInterceptor;
+//    }
+
     public boolean isPago() {
         return StatusPedido.PAGO.equals(status);
     }
 
+    //@PrePersist
+    //@PreUpdate
     public void calcularTotal() {
         if (itens != null) {
             total = itens.stream().map(item -> new BigDecimal(item.getQuantidade()).multiply(item.getPrecoProduto()))
