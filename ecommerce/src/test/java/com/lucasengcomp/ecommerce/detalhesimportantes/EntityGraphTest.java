ba66d6a71eb37d26864b80af6394a84c32b4965f
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-03-28 23:03:29 -0300

    :test_tube: Entendendo e configurando um Entity Graph

---
+++ ecommerce/src/test/java/com/lucasengcomp/ecommerce/detalhesimportantes/EntityGraphTest.java
@@ -0,0 +1,33 @@
+package com.lucasengcomp.ecommerce.detalhesimportantes;
+
+import com.lucasengcomp.ecommerce.EntityManagerTest;
+import com.lucasengcomp.ecommerce.model.Pedido;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.EntityGraph;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class EntityGraphTest extends EntityManagerTest {
+
+    @Test
+    public void buscarAtributosEssenciaisDePedido() {
+        EntityGraph<Pedido> entityGraph = entityManager.createEntityGraph(Pedido.class);
+        entityGraph.addAttributeNodes(
+                "dataCriacao", "status", "total", "notaFiscal");
+        /*
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("javax.persistence.fetchgraph", entityGraph);
+//        properties.put("javax.persistence.loadgraph", entityGraph);
+        Pedido pedido = entityManager.find(Pedido.class, 1, properties);
+        Assert.assertNotNull(pedido);
+        */
+
+        TypedQuery<Pedido> typedQuery = entityManager
+                .createQuery("SELECT p FROM Pedido p", Pedido.class);
+        typedQuery.setHint("javax.persistence.fetchgraph", entityGraph);
+        List<Pedido> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+}
