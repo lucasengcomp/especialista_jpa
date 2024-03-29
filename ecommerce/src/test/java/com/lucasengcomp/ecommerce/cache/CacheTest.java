60fbc3dbc0e5752683a35006b4aef764f2ca5410
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-03-29 15:31:21 -0300

    :test_tube: Verificando se uma entidade está no cache

--- ecommerce/src/test/java/com/lucasengcomp/ecommerce/cache/CacheTest.java
+++ ecommerce/src/test/java/com/lucasengcomp/ecommerce/cache/CacheTest.java
@@ -2,6 +2,7 @@ package com.lucasengcomp.ecommerce.cache;
 
 import com.lucasengcomp.ecommerce.model.Pedido;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -23,6 +24,21 @@ public class CacheTest {
     @AfterClass
     public static void tearDownAfterClass() {
         entityManagerFactory.close();
+    }
+
+    @Test
+    public void verificarSeEstaNoCache() {
+        Cache cache = entityManagerFactory.getCache();
+
+        EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+
+        System.out.println("Buscando a partir da instância 1:");
+        entityManager1
+                .createQuery("select p from Pedido p", Pedido.class)
+                .getResultList();
+
+        Assert.assertTrue(cache.contains(Pedido.class, 1));
+        Assert.assertTrue(cache.contains(Pedido.class, 2));
     }
 
     @Test
