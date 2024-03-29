6f83c98e5cf4f35267eb52c90ffbc02143f5b76c
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2024-03-29 18:54:27 -0300

    :test_tube: Modos de cache e a anotação @Cacheable

--- ecommerce/src/main/java/com/lucasengcomp/ecommerce/model/Pedido.java
+++ ecommerce/src/main/java/com/lucasengcomp/ecommerce/model/Pedido.java
@@ -10,10 +10,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PastOrPresent;
-import javax.validation.constraints.Positive;
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
